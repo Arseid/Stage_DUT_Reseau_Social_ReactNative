761e590868ab19f7d20a7379123fb19cf3984a5e
NicoG238 <nicolas.giliberto@etu.unice.fr>
2022-04-12 14:14:07 +0200

    bordure

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app</Text>
+      <Text></Text>
       <StatusBar style="auto" />
+      <View style={{ flex: 2, borderWidth: 2, borderColor: 'grey'}}/>
+      <View style={{ flex: 1, borderWidth: 2, borderColor: 'grey'}}/>
     </View>
+    
   );
 }
 
@@ -15,7 +19,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    marginTop: 15,
+    marginLeft: 5,
+    marginRight: 5,
+    marginBottom: 15,
     justifyContent: 'center',
   },
+
 });
