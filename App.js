f75c5522c22beb6788d3c173fc2dca554069d133
NicoG238 <nicolas.giliberto@etu.unice.fr>
2022-04-12 15:32:46 +0200

    ajout texte (à corriger)

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import React from 'react';
+import { color } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text></Text>
+      
+      <Text style={{height:15, textAlign: 'center', fontSize: 20,marginTop: 20}}>Bienvenue !</Text>
       <StatusBar style="auto" />
-      <View style={{ flex: 2, borderWidth: 2, borderColor: 'grey'}}/>
+      <View style={{ flex: 3, borderWidth: 2, borderColor: 'grey',justifyContent: 'center', alignItems: 'center'}}/>
       <View style={{ flex: 1, borderWidth: 2, borderColor: 'grey'}}/>
-    </View>
+      
+  
+     
+   
+</View>
     
+     
+     
   );
 }
 
@@ -19,11 +27,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    marginTop: 15,
     marginLeft: 5,
     marginRight: 5,
     marginBottom: 15,
     justifyContent: 'center',
   },
+
+  Text: {
+    
+    
+  }
 
 });
