ed76352ec5066b70371ffc261660231237deae8f
NicoG238 <nicolas.giliberto@etu.unice.fr>
2022-04-12 18:50:57 +0200

    inscription

--- App.js
+++ App.js
@@ -1,22 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Pressable , Alert } from 'react-native';
 import React from 'react';
-import { color } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      
-      <Text style={{height:15, textAlign: 'center', fontSize: 20,marginTop: 20}}>Bienvenue !</Text>
-      <StatusBar style="auto" />
-      <View style={{ flex: 3, borderWidth: 2, borderColor: 'grey',justifyContent: 'center', alignItems: 'center'}}/>
-      <View style={{ flex: 1, borderWidth: 2, borderColor: 'grey'}}/>
-      
-  
-     
-   
-</View>
+
+      <View style={styles.form}>
+        <View>
+          <Text style={styles.title}>Lorem Ipsum</Text>
+          <TextInput style={styles.infoInput} placeholder="Numéro de téléphone ou email"/>
+          <TextInput style={styles.infoInput} placeholder="Mot de passe"/>
+          <Pressable style={styles.button}>
+            <Text style={styles.averageText}>Se connecter</Text>
+          </Pressable>
+        </View>
+      </View> 
+
+      <View style={styles.subscription}>
+
+      </View>
+
+
+
+    </View>
     
      
      
@@ -26,16 +34,50 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    marginLeft: 5,
-    marginRight: 5,
-    marginBottom: 15,
-    justifyContent: 'center',
+    backgroundColor: '#e6e6e6',
   },
 
-  Text: {
-    
-    
+  form:{
+    flex:1,
+    borderColor:"white",
+    borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  title:{
+    height:15, 
+    justifyContent: 'flex-end', 
+    alignItems: 'flex-end',
+    textAlign: 'center', 
+    fontSize: 30,
+    margin:50,
+  },
+
+  infoInput:{
+    width: 300,
+    height: 40, 
+    borderWidth: 1,
+    padding:5,
+    margin:5
+  },
+
+  button:{
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    borderRadius: 4,
+    backgroundColor: '#ffaf7a',
+    padding:5,
+    margin:5
+  },
+
+  averageText:{
+    fontSize: 20,
+  },
+
+  subscription:{
+
   }
 
 });
