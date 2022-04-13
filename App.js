8545c313ea40b23458137efaee8d7142859f87b7
Arseid <yvesrobert.estrada@gmail.com>
2022-04-13 10:33:15 +0200

    Login done

--- App.js
+++ App.js
@@ -7,42 +7,68 @@ export default function App() {
   return (
     <View style={styles.container}>
 
-      <View style={styles.form}>
-        <View>
+      <View style={styles.upside}>
+
+        <View style={styles.form}>
           <Text style={styles.title}>Lorem Ipsum</Text>
           <TextInput style={styles.infoInput} placeholder="Numéro de téléphone ou email"/>
           <TextInput style={styles.infoInput} placeholder="Mot de passe"/>
           <Pressable style={styles.button}>
             <Text style={styles.averageText}>Se connecter</Text>
           </Pressable>
+          <Text style={styles.hyperlinkText}>Mot de passe oublié?</Text>
         </View>
+
+        <View style={styles.subscription}>
+          <Text style={styles.averageText}>Vous n'avez pas de compte?</Text>
+          <Text style={styles.hyperlinkText} onPress={() => Linking.openURL('https://patelkrunal008.medium.com/navigate-from-one-screen-to-another-in-react-native-navigation-v5-69da74d5c676')}>
+            Inscrivez-vous!
+          </Text>
+        </View>
+
       </View> 
 
-      <View style={styles.subscription}>
-
+      <View style={styles.downside}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel hendrerit elit. Nunc et laoreet quam. Praesent finibus tristique elit. Phasellus id velit consequat nulla tempus maximus sit amet a lorem. Morbi eget elit auctor, mollis lorem ut, egestas leo. Nullam euismod pulvinar risus at iaculis. Fusce commodo turpis vitae velit feugiat lacinia. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam fermentum tempor luctus. Duis pretium vulputate ex, ut consequat dui. Praesent eu turpis non elit vehicula tincidunt a et neque. Pellentesque lobortis imperdiet orci, ullamcorper scelerisque massa consequat vitae. Aenean vel dui sapien. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam at massa sit amet magna fringilla sagittis id quis purus. Mauris id scelerisque sapien.
       </View>
-
-
-
     </View>
-    
-     
-     
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#e6e6e6',
+    backgroundColor: '#eeeeee',
+  },
+
+  upside:{
+    flex:4,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+   downside:{
+    flex:1,
+    backgroundColor:"#FFFAF0",
   },
 
   form:{
-    flex:1,
-    borderColor:"white",
-    borderWidth: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    marginTop:40,
+    borderColor:"#FFFAF0",
+    backgroundColor:"#FFFAF0",
+    borderWidth:5,
+    borderRadius: 10,
+    padding:10,
+  },
+
+  subscription:{
+    marginTop:15,
+    padding:10,
+    borderWidth:5,
+    borderRadius: 10,
+    flexDirection: 'row',
+    borderColor:"#FFFAF0",
+    backgroundColor:"#FFFAF0",
   },
 
   title:{
@@ -50,8 +76,8 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end', 
     alignItems: 'flex-end',
     textAlign: 'center', 
-    fontSize: 30,
-    margin:50,
+    fontSize: 35,
+    marginBottom:80,
   },
 
   infoInput:{
@@ -64,8 +90,6 @@ const styles = StyleSheet.create({
 
   button:{
     alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 12,
     borderRadius: 4,
     backgroundColor: '#ffaf7a',
     padding:5,
@@ -73,11 +97,15 @@ const styles = StyleSheet.create({
   },
 
   averageText:{
-    fontSize: 20,
+    fontSize: 15,
+    textAlign:"center",
+    margin:5
   },
 
-  subscription:{
-
+  hyperlinkText:{
+    fontSize: 15,
+    textAlign:"center",
+    margin:5,
+    color:"#315399",
   }
-
 });
